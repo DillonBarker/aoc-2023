0b18ae1dcfd7467d904d4a2f65d9808cd13b7c8a
dillon.barker <dillon.barker@testevolve.com>
2023-12-06 06:18:41 +0000

    part 2 day 6 with quadratic eq solves fast

--- src/main/kotlin/net/dill/y2023/day6/Day6.kt
+++ src/main/kotlin/net/dill/y2023/day6/Day6.kt
@@ -3,6 +3,9 @@ package net.dill.y2023.day6
 import net.dill.println
 import net.dill.readInput
 import net.dill.readTestInput
+import kotlin.math.absoluteValue
+import kotlin.math.ceil
+import kotlin.math.sqrt
 
 const val DAY = "day6"
 
@@ -28,32 +31,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        val timeLine = input[0].split(": ")[1].split(" ").filter { it.isNotEmpty()}
-        val distanceLine = input[1].split(": ")[1].split(" ").filter { it.isNotEmpty()}
+        val timeLine = input[0].split(": ")[1].split(" ").filter { it.isNotEmpty() }
+        val distanceLine = input[1].split(": ")[1].split(" ").filter { it.isNotEmpty() }
 
         val raceTime = timeLine.joinToString(separator = "").toLong()
         val raceDistance = distanceLine.joinToString(separator = "").toLong()
 
-        var count = 0L
-
-        println(raceDistance downTo 1)
-
-        for (speed in raceDistance downTo 1) {
-            val timeLeft = raceTime - speed
-            val distanceTravelled = timeLeft * speed
-            if (distanceTravelled > raceDistance) count++
-        }
-
-        return count
+        return calculateResult(raceTime, raceDistance)
     }
 
     val testInput1 = readTestInput(DAY, "a")
     val testInput2 = readTestInput(DAY, "b")
-//    check(part1(testInput1) == 288)
+    check(part1(testInput1) == 288)
     check(part2(testInput2) == 71503L)
 
 
     val input = readInput(DAY)
-//    part1(input).println()
+    part1(input).println()
     part2(input).println()
 }
+
+// found a quadratic eq to do the maths!
+fun calculateResult(time: Long, distance: Long): Long {
+    val firstWin = ceil((time + sqrt(time.toDouble() * time - 4 * (distance + 1.0))) / 2).toLong()
+    return (time - 2 * firstWin + 1).absoluteValue
+}
+
