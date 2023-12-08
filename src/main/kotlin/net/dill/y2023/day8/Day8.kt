f46a9eff99d010a6c42cfb0dc6567a7053a89515
dillon.barker <dillon.barker@testevolve.com>
2023-12-08 06:40:57 +0000

    need to make a utilities folder which has the useful methods such as lcm

--- src/main/kotlin/net/dill/y2023/day8/Day8.kt
+++ src/main/kotlin/net/dill/y2023/day8/Day8.kt
@@ -31,7 +31,7 @@ fun main() {
         return steps
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<String>): Long {
         val instructions = input[0]
         val network = input.drop(1).drop(1)
 
@@ -41,39 +41,41 @@ fun main() {
             networkMaps[node[0]] = Pair(node[1].split(", ")[0].replace("(", ""), node[1].split(", ")[1].replace(")", ""))
         }
 
-        var steps = 0
-        val locations = networkMaps.filter { it.key.endsWith("A") }.keys.toList().toMutableList()
-
-        var end = false
-
-        while (!end) {
-            instructions.forEach { instruction ->
-                locations.forEachIndexed { index, location ->
-                    when (instruction) {
-                        'L' -> {
-                            locations[index] = networkMaps[location]?.first!!
-                        }
-                        'R' -> {
-                            locations[index] = networkMaps[location]?.second!!
-                        }
+        val counts = networkMaps.keys.filter { it.endsWith("A") }.map { startingPoint ->
+            var current = startingPoint
+            var count = 0L
+            while (!current.endsWith("Z")) {
+                instructions.forEach { instruction ->
+                    current = when (instruction) {
+                        'L' -> networkMaps[current]!!.first
+                        'R' -> networkMaps[current]!!.second
+                        else -> println("PANIC").toString()
                     }
                 }
-
-                if (locations.all { it.endsWith("Z") }) end = true
-                steps++
-                println(locations)
+                count += instructions.length
             }
+            count
         }
-        return steps
+
+        return counts.reduce { acc, i -> leastCommonMultiple(acc, i) }
     }
 
     val testInput1 = readTestInput(DAY, "a")
     val testInput2 = readTestInput(DAY, "b")
     check(part1(testInput1) == 6)
-    check(part2(testInput2) == 6)
+    check(part2(testInput2) == 6L)
 
 
     val input = readInput(DAY)
     part1(input).println()
     part2(input).println()
+}
+
+fun leastCommonMultiple(a: Long, b: Long): Long {
+    return a * b / greatestCommonDivisor(a, b)
+}
+
+tailrec fun greatestCommonDivisor(a: Long, b: Long): Long {
+    return if (b == 0L) a
+    else greatestCommonDivisor(b, a % b)
 }
